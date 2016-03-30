4688c9375bb954265489a8dc8fe0d25ef57a6cbb
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2016-03-30 21:04:54 +0200

    Adding default template values.

--- webeval/__init__.py
+++ webeval/__init__.py
@@ -13,12 +13,15 @@ def inject_default_values():
 		"topic": request.form.get('topic', ''),
 		"medium": request.form.get('medium', ''),
 		"timing": request.form.get('timing', ''),
+		"ordering": request.form.get('ordering', ''),
 		"group": request.form.get('group', ''),
+		"verification": database.getVerificationFromMap("", request.form),
 		"verification_require": database.getVerificationFromMap("require", request.form),
 		"verification_exclude": database.getVerificationFromMap("exclude", request.form),
 		"topics": database.listTopicDict,
 		"groups": database.listGroupDict,
 		"verifications": database.getVerifications,
+		"orderings": database.listOrderingDict,
 		"core_stats": stats.gatherCoreData,
 		"isSet": lambda single, value: (single & value) == single
 	}
